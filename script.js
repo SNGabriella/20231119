c57bc0911d302b9391bcced7c6fb8f1b5202787f
sngabriella <ngabriella26@gmail.com>
2023-11-19 15:54:59 +0100

    módosított változat

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// VAN - E NEGATíV SZÁM EGY TÖMBBEN!!! 
+// .feladat: VAN - E NEGATíV SZÁM EGY TÖMBBEN!!! 
 function VanENegativ(szamok) {
     var i = 0;
     while (i < szamok.length && !(szamok[i] < 0)) {
@@ -6,7 +6,7 @@ function VanENegativ(szamok) {
     }
     return i < szamok.length;
 }
-// Kör kerületét és területét számítja ki parameterül kapott sugarból, tupple-t alkamazva??!!
+// 2.feladat: Kör kerületét és területét számítja ki paraméterként kapott sugárból, tupple-t alkalmazva??!!
 function KorSzamitas(radius) {
     var kerulet = 2 * radius * radius;
     var terulet = Math.PI * radius * radius;
@@ -15,3 +15,45 @@ function KorSzamitas(radius) {
 console.log(VanENegativ([5, 12, 3, 6, 9]));
 console.log(VanENegativ([5, 12, -3, 6, 9]));
 console.log(KorSzamitas(5));
+// 4.feladat: Alprogram, ami kiválasztja egy autó tömbből a legkisebb hengerűrtartalmút
+function LegkisebbHenger(autok) {
+    var minhenger = autok[0];
+    for (var i = 0; i < autok.length; i++) {
+        if (autok[i].Hengerurtartalom < minhenger.Hengerurtartalom) {
+            minhenger = autok[i];
+        }
+    }
+    return minhenger;
+}
+// Autó tömbből megadja a benzinesek darabszámát
+function Benzinesek(autok) {
+    var benzinesDb = 0;
+    for (var i = 0; i < autok.length; i++) {
+        if (autok[i].BenzinesE == true) {
+            benzinesDb++;
+        }
+    }
+    return benzinesDb;
+}
+// Tesztelése az autóknak
+var A1 = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    Hengerurtartalom: 1100,
+    BenzinesE: true
+};
+var A2 = {
+    gyarto: "Ford",
+    tipus: "K",
+    Hengerurtartalom: 1000,
+    BenzinesE: false
+};
+var A3 = {
+    gyarto: "Dacia",
+    tipus: "Duster",
+    Hengerurtartalom: 1600,
+    BenzinesE: true
+};
+var autok = [A1, A2, A3];
+console.log(Benzinesek(autok));
+console.log(LegkisebbHenger(autok));
--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-// VAN - E NEGATíV SZÁM EGY TÖMBBEN!!! 
+// .feladat: VAN - E NEGATíV SZÁM EGY TÖMBBEN!!! 
 
 function VanENegativ(szamok : number []):boolean{
     var i:number = 0;
@@ -11,10 +11,10 @@ function VanENegativ(szamok : number []):boolean{
 }
 
 
-// Kör kerületét és területét számítja ki parameterül kapott sugarból, tupple-t alkamazva??!!
+// 2.feladat: Kör kerületét és területét számítja ki paraméterként kapott sugárból, tupple-t alkalmazva??!!
 
 
-function KorSzamitas(radius : number): [number, number]{
+function KorSzamitas(radius : number): [number, number]{ // ez a visszatérő jelenti, hogy tuple-t alkalmazunk!!! (?!)
 
    var kerulet = 2 * radius * radius;
    var terulet = Math.PI * radius * radius;
@@ -27,7 +27,7 @@ console.log(VanENegativ([5,12,-3,6,9]));
 console.log(KorSzamitas(5));
 
 
-// Interface Auto néven
+// 3. feladat: Interface Auto néven
 
 interface Auto{
     gyarto : string;
@@ -36,4 +36,59 @@ interface Auto{
     BenzinesE: boolean;
 }
 
+// 4.feladat: Alprogram, ami kiválasztja egy autó tömbből a legkisebb hengerűrtartalmút
 
+function LegkisebbHenger(autok : Auto []) : Auto{
+    let minhenger:Auto = autok[0];
+
+    for(var i = 0; i < autok.length; i++){
+        if(autok[i].Hengerurtartalom < minhenger.Hengerurtartalom){
+            minhenger = autok[i];
+        }  
+    }
+    return minhenger;
+}
+
+
+// Autó tömbből megadja a benzinesek darabszámát
+
+function Benzinesek(autok : Auto []):number{
+
+    var benzinesDb = 0;
+    
+    for(var i = 0; i < autok.length; i++){
+        if(autok[i].BenzinesE == true){
+            benzinesDb++;
+        }
+    }
+
+    return benzinesDb;
+}
+
+// Tesztelése az autóknak
+
+var A1: Auto = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    Hengerurtartalom: 1100,
+    BenzinesE : true
+};
+
+var A2: Auto = {
+    gyarto: "Ford",
+    tipus: "K",
+    Hengerurtartalom: 1000,
+    BenzinesE : false
+}
+
+var A3: Auto = {
+    gyarto: "Dacia",
+    tipus: "Duster",
+    Hengerurtartalom: 1600,
+    BenzinesE : true
+}
+
+var autok : Auto [] = [A1, A2, A3];
+
+console.log(Benzinesek(autok));
+console.log(LegkisebbHenger(autok));
